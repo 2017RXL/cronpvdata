7f55a05c9c076c0c5710a70aa9706d5acbc74429
ruixule <1414100586@qq.com>
2020-01-30 11:34:00 +0800

    修改定时器的 时间触发点

--- main.go
+++ main.go
@@ -5,20 +5,20 @@ import (
 	"github.com/robfig/cron"
 )
 
-
 func main() {
+	cronData()
+}
+func cronData() {
 	model.Logger.Info("定时修改数据任务已经启动！！")
 	go cronDoWork()
 	var str chan string
 	d := <-str
 	model.Logger.Info(d)
-
-
 }
 func cronDoWork() {
 	c := cron.New()
-	c.AddFunc("0 */1 * * * ?", func() {//每个1分钟执行一次
-	//c.AddFunc("0 0 2 * * ?", func() { //每天2点执行一次
+	//c.AddFunc("0 */1 * * * ?", func() { //每个1分钟执行一次
+	c.AddFunc("0 0 2 * * ?", func() { //每天2点执行一次
 		model.Logger.Info("开始更新数据了")
 		model.UpdateData()
 		model.Logger.Info("更新数据完毕了")
