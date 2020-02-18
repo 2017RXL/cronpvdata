779cb5af5ed3a6b7b681331365180f7afd60b161
ruixule <1414100586@qq.com>
2020-02-18 08:22:00 +0800

    优化永久执行 逻辑

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	cronData()
-}
-func cronData() {
 	model.Logger.Info("定时修改数据任务已经启动！！")
+	go cronDoWork()
+	//主程序永久执行
+	select {}
+}
+
+//以前旧的方案，采用通道堵塞实现 永久执行
+func cronData() {
 	go cronDoWork()
 	var str chan string
 	d := <-str
@@ -17,8 +21,8 @@ func cronData() {
 }
 func cronDoWork() {
 	c := cron.New()
-	//c.AddFunc("0 */1 * * * ?", func() { //每个1分钟执行一次
-	c.AddFunc("0 0 2 * * ?", func() { //每天2点执行一次
+	c.AddFunc("0 */1 * * * ?", func() { //每个1分钟执行一次
+		//c.AddFunc("0 0 2 * * ?", func() { //每天2点执行一次
 		model.Logger.Info("开始更新数据了")
 		model.UpdateData()
 		model.Logger.Info("更新数据完毕了")
